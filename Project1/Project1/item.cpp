769b56eb9e8fbed0b94dea9c1adeb949d96c08e9
Minho99 <40349868+Minho99@users.noreply.github.com>
2018-06-19 17:17:02 +0900

    hi

--- Project1/Project1/item.cpp
+++ Project1/Project1/item.cpp
@@ -1,6 +1,6 @@
 #include "item.h"
 #include <iostream>
-#include <string>
+
 
 using namespace std;
 
@@ -8,12 +8,8 @@ using namespace std;
 
 item::item()
 {
-	string hi;
-	cout << " 민호 : 안녕하세요 여러분 " << endl;
-	cout << " 민호의 인사를 받아주세요" << endl;
-	cin >> hi;
 	
-	cout << "이걸 안받아줘 버리나 ??" << endl;
+	cout << " 민호 : 안녕하세요 여러분 " << endl;
 
 }
 
--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+
 using namespace std;
-int main()
+int main(void)
 {
 	cout << "¾È³ç!" << endl;
 	cout << "ÇÏÀÌ!!"<< endl;
