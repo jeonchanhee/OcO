a3aab3f9913a5a2aee09915f3b175163c29bb204
maple9420 <maple9420@naver.com>
2018-06-20 17:30:23 +0900

    내클래스만듬~~

---
+++ Project1/Project1/ilgwon.cpp
@@ -0,0 +1,15 @@
+#include "ilgwon.h"
+#include <iostream>
+
+using namespace std;
+
+
+ilgwon::ilgwon()
+{
+	cout << "¾È³ç~~" << endl;
+}
+
+
+ilgwon::~ilgwon()
+{
+}
---
+++ Project1/Project1/ilgwon.h
@@ -0,0 +1,8 @@
+#pragma once
+class ilgwon
+{
+public:
+	ilgwon();
+	~ilgwon();
+};
+
