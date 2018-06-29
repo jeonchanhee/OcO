66b9638465904d01ed08cdbf854f74fb995c788d
전찬희 <wjscskgml@naver.com>
2018-06-29 15:50:01 +0900

    DC

--- 던그리드/playGround.cpp
+++ 던그리드/playGround.cpp
@@ -38,7 +38,7 @@ void playGround::update(void)
 {
 	gameNode::update();
 	//SCENEMANAGER->update();
-	_mapTool->update();
+	_mapTool->update();  
 //	if (KEYMANAGER->isStayKeyDown(VK_LEFT)) CAMERAMANAGER->setCameraX(CAMERAMANAGER->getCameraX() - 5);
 
 }
@@ -47,7 +47,7 @@ void playGround::render(void)
 {
 	
 	//흰색도화지 한 장 깔아둔다
-	PatBlt(CAMERAMANAGER->getCameraDC()->getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
+	PatBlt(DC, 0, 0, WINSIZEX, WINSIZEY, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
 	//============== 이 위로는 건드리지 말자 ==============
 	
 	_mapTool->render();
@@ -55,6 +55,7 @@ void playGround::render(void)
 
 	
 	//================이 밑으로도 건드리지 말자 =============
+	IMAGEMANAGER->render("cursor", DC, _ptMouse.x, _ptMouse.y);
 	CAMERAMANAGER->getCameraDC()->render(getHDC() , 0 , 0 , CAMERAMANAGER->getCameraRc().left, CAMERAMANAGER->getCameraRc().top, CAMERAMANAGER->getCameraRc().right, CAMERAMANAGER->getCameraRc().bottom); 
 	// 맨마지막으로 카메라 매니저 의 DC를 그려줍니다.
 	// CAMERAMANAGER->setCameraX()  CAMERAMANAGER->setCameraY() 를 이용하면   X ,Y 에  WINSIZEX , WINSIZEY 만큼 화면에 그려주고있는 렉트를 이동시킬 수 있음 .
