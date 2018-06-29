a8a92a96879419275c8f0a71e1c57b442b5b4760
주 효원 <222kg@naver.com>
2018-06-29 12:20:22 +0900

    효웒ㅎㅎ

--- 던그리드/Enemy.h
+++ 던그리드/Enemy.h
@@ -1,10 +1,25 @@
 #pragma once
 #include "gameNode.h"
+
 class Enemy :	public gameNode
 {
+protected:
+	image* _img;				//적 이미지
+	RECT _rc;				//적 렉트
+	int _frameX, _frameY;	//프레임 번호
+
+	float _x, _y;			//적 x, y좌표
+	float _speed, _angle;	//스피드와 각도
+
+	int _count;				//프레임 카운트
+	int _fireCount;			//발사 카운트
+	int _rndFireCount;		//랜덤하게 발사할 카운트
+
+
 public:
 
 	HRESULT init();
+	HRESULT init(const char* imageName, POINT position, float angle, float speed);
 	void release();
 	void update();
 	void render();
---
+++ 던그리드/enemyManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "gameNode.h"
+class enemyManager : public gameNode
+{
+public:
+	enemyManager();
+	~enemyManager();
+};
+
