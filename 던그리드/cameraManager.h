37c25a6ae5c8dfe1175fae34a793b413ef4f909e
민호 김 <kmh990407@gmail.com>
2018-06-29 10:41:43 +0900

    cameraManager - 민호;

---
+++ 던그리드/cameraManager.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "singletonBase.h"
+#include "image.h"
+
+
+// 전체 맵 사이즈 입니다   수정 부우탁 드립니다
+#define BACKGROUNDSIZEX 2000
+#define BACKGROUNDSIZEY 2000
+
+class cameraManager  : public  singletonBase<cameraManager>
+{
+private:
+	
+	image * _cameraDC;
+	RECT _cameraRc;
+	float _cameraX, _cameraY;
+
+public:
+	HRESULT init();
+	void update();
+	void release();
+	  
+
+	void setCameraX(float x) { _cameraX = x; }
+	void setCameraY(float y) { _cameraY = y; }
+	float getCameraX() { return _cameraX; }
+	float getCameraY() { return _cameraY; }
+
+
+	image * getCameraDC() { return _cameraDC; }
+	RECT getCameraRc() { return _cameraRc; }
+	cameraManager();
+	~cameraManager();
+};
+
