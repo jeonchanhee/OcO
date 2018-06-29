3c47b60d16d833f3bd154097442f4b779341ba1e
윤은지 <eunje15@naver.com>
2018-06-29 17:14:25 +0900

    다이얼로그_은지

--- 던그리드/DialogStore.cpp
+++ 던그리드/DialogStore.cpp
@@ -42,35 +42,35 @@ void DialogStore::update()
 
 void DialogStore::render()
 {
-	Rectangle(getMemDC(), _click.left, _click.top, _click.right, _click.bottom);
+	Rectangle(DC, _click.left, _click.top, _click.right, _click.bottom);
 
 	for (int i = 0; i < 3; i++)
 	{
-		Rectangle(getMemDC(), _rc[i].left, _rc[i].top, _rc[i].right, _rc[i].bottom);
-		Rectangle(getMemDC(), _button[i].left, _button[i].top, _button[i].right, _button[i].bottom);
+		Rectangle(DC, _rc[i].left, _rc[i].top, _rc[i].right, _rc[i].bottom);
+		Rectangle(DC, _button[i].left, _button[i].top, _button[i].right, _button[i].bottom);
 	}
 
 	HFONT font, oldFont;
 	font = CreateFont(80, 0, 0, 0, 100, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, TEXT("家具官弗9"));
-	oldFont = (HFONT)SelectObject(getMemDC(), font);
-	SetTextColor(getMemDC(), RGB(255, 94, 0));
-	//DrawText(getMemDC(), _dialog[(int)_training][_idY].c_str(), strlen(_dialog[(int)_training][_idY].c_str()), &_rc[0], DT_VCENTER);
-	DrawText(getMemDC(), _name.c_str(), strlen(_name.c_str()), &_rc[0], DT_VCENTER);
-	SelectObject(getMemDC(), oldFont);
+	oldFont = (HFONT)SelectObject(DC, font);
+	SetTextColor(DC, RGB(255, 94, 0));
+	//DrawText(DC, _dialog[(int)_training][_idY].c_str(), strlen(_dialog[(int)_training][_idY].c_str()), &_rc[0], DT_VCENTER);
+	DrawText(DC, _name.c_str(), strlen(_name.c_str()), &_rc[0], DT_VCENTER);
+	SelectObject(DC, oldFont);
 	DeleteObject(font);
 
 	font = CreateFont(50, 0, 0, 0, 100, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, TEXT("家具官弗9"));
-	oldFont = (HFONT)SelectObject(getMemDC(), font);
-	SetTextColor(getMemDC(), RGB(0, 0, 0));
+	oldFont = (HFONT)SelectObject(DC, font);
+	SetTextColor(DC, RGB(0, 0, 0));
 	string str = _dialog[(int)_store][_idY].substr(0, _idX);
-	//DrawText(getMemDC(), _dialog[(int)_training][_idY].c_str(), strlen(_dialog[(int)_training][_idY].c_str()), &_rc[0], DT_VCENTER);
-	DrawText(getMemDC(), str.c_str(), strlen(str.c_str()), &_rc[1], DT_VCENTER);
-	SelectObject(getMemDC(), oldFont);
+	//DrawText(DC, _dialog[(int)_training][_idY].c_str(), strlen(_dialog[(int)_training][_idY].c_str()), &_rc[0], DT_VCENTER);
+	DrawText(DC, str.c_str(), strlen(str.c_str()), &_rc[1], DT_VCENTER);
+	SelectObject(DC, oldFont);
 	DeleteObject(font);
 
 	char str1[128];
 	sprintf_s(str1, "size : %d, count : %d", _dialog[(int)_store][_idY].size(), _count / 10);
-	TextOut(getMemDC(), 300, 100, str1, strlen(str1));
+	TextOut(DC, 300, 100, str1, strlen(str1));
 
 }
 
