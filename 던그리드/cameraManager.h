3cff9d003b5749ef2991f3b8dc6203a5cce3d965
문희진 <40350423+orange492@users.noreply.github.com>
2018-07-02 10:50:10 +0900

    맵카메라

--- 던그리드/cameraManager.h
+++ 던그리드/cameraManager.h
@@ -7,13 +7,19 @@
 #define BACKGROUNDSIZEX 3000
 #define BACKGROUNDSIZEY 3000
 
+#define CAMERA2X 1100
+#define CAMERA2Y 1080
+
 class cameraManager  : public  singletonBase<cameraManager>
 {
 private:
 	
 	image * _cameraDC;
+	image * _cameraDC2;
 	RECT _cameraRc;
+	RECT _cameraRc2;
 	float _cameraX, _cameraY;
+	float _cameraX2, _cameraY2;
 
 public:
 	HRESULT init();
@@ -22,13 +28,19 @@ public:
 	  
 
 	void setCameraX(float x) { _cameraX = x; }
+	void setCameraX2(float x) { _cameraX2 = x; }
 	void setCameraY(float y) { _cameraY = y; }
+	void setCameraY2(float y) { _cameraY2 = y; }
 	float getCameraX() { return _cameraX; }
+	float getCameraX2() { return _cameraX2; }
 	float getCameraY() { return _cameraY; }
+	float getCameraY2() { return _cameraY2; }
 
 
 	image * getCameraDC() { return _cameraDC; }
+	image * getCameraDC2() { return _cameraDC2; }
 	RECT getCameraRc() { return _cameraRc; }
+	RECT getCameraRc2() { return _cameraRc2; }
 	cameraManager();
 	~cameraManager();
 };
--- 던그리드/playGround.cpp
+++ 던그리드/playGround.cpp
@@ -37,16 +37,19 @@ void playGround::release(void)
 void playGround::update(void)	
 {
 	gameNode::update();
-	/*_mapTool->update();
-	_player->update();*/
-	SCENEMANAGER->update();
+	_mapTool->update();
+	_player->update();
+	//SCENEMANAGER->update();
 
 	if (KEYMANAGER->isOnceKeyDown(VK_F1))
 	{
 		SOUNDMANAGER->play("title");
 	}
 
-//	if (KEYMANAGER->isStayKeyDown(VK_LEFT)) CAMERAMANAGER->setCameraX(CAMERAMANAGER->getCameraX() - 5);
+	if (KEYMANAGER->isStayKeyDown('D')) CAMERAMANAGER->setCameraX2(CAMERAMANAGER->getCameraX2() + 5);
+	if (KEYMANAGER->isStayKeyDown('S')) CAMERAMANAGER->setCameraY2(CAMERAMANAGER->getCameraY2() + 5);
+	if (KEYMANAGER->isStayKeyDown('A')) CAMERAMANAGER->setCameraX2(CAMERAMANAGER->getCameraX2() - 5);
+	if (KEYMANAGER->isStayKeyDown('W')) CAMERAMANAGER->setCameraY2(CAMERAMANAGER->getCameraY2() - 5);
 
 }
 
@@ -55,17 +58,21 @@ void playGround::render(void)
 	
 	//흰색도화지 한 장 깔아둔다
 	//PatBlt(DC, 0, 0, WINSIZEX, WINSIZEY, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
-	PatBlt(DC, 0, 0, WINSIZEX, WINSIZEY, BLACKNESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
+	PatBlt(DC, 0, 0, WINSIZEX, WINSIZEY, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
+	PatBlt(DC2, 0, 0, 3000, 3000, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
 	//============== 이 위로는 건드리지 말자 ==============
 	
-	/*_mapTool->render();
-	_player->render();*/
+	_mapTool->render();
+	//_player->render();
 	
-	SCENEMANAGER->render();
+	//SCENEMANAGER->render();
 
 	//================이 밑으로도 건드리지 말자 =============
+	
+	CAMERAMANAGER->getCameraDC2()->render(DC, 0, 0, CAMERAMANAGER->getCameraRc2().left, CAMERAMANAGER->getCameraRc2().top, CAMERA2X, CAMERA2Y);
 	IMAGEMANAGER->render("cursor", DC, _ptMouse.x, _ptMouse.y);
 	CAMERAMANAGER->getCameraDC()->render(getHDC() , 0 , 0 , CAMERAMANAGER->getCameraRc().left, CAMERAMANAGER->getCameraRc().top, CAMERAMANAGER->getCameraRc().right, CAMERAMANAGER->getCameraRc().bottom); 
+	
 	// 맨마지막으로 카메라 매니저 의 DC를 그려줍니다.
 	// CAMERAMANAGER->setCameraX()  CAMERAMANAGER->setCameraY() 를 이용하면   X ,Y 에  WINSIZEX , WINSIZEY 만큼 화면에 그려주고있는 렉트를 이동시킬 수 있음 .
 	// 그리고 모든 렌더의 hdc 는 CAMERAMANAGER->getCameraDC()->getMemDC() 로 해주어야함 !~~!!$!$!#ㄸ!#ㄸ!$!$#@$!@#@!
