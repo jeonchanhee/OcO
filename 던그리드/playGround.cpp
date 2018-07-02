7c5acf1a419b67af0161ec70086705aa33b4b5ae
문희진 <40350423+orange492@users.noreply.github.com>
2018-07-02 11:16:48 +0900

    플그에 이넘 추가

--- 던그리드/playGround.cpp
+++ 던그리드/playGround.cpp
@@ -8,6 +8,9 @@ playGround::~playGround(){}
 
 HRESULT playGround::init(void)	
 {
+	mode = 타이틀;				//본인이 편집하는 부분으로 이넘에 추가하고 수정해서 사용하기!!
+
+
 	gameNode::init(true);
 	Image_init();
 	Sound_init();
@@ -20,8 +23,28 @@ HRESULT playGround::init(void)
 	SCENEMANAGER->addScene("던전", new dungeonScene);
 	SCENEMANAGER->addScene("도그본", new EnemyManager);
 	SCENEMANAGER->addScene("대사씬", new Dialog);
-	//SCENEMANAGER->changeScene("도그본");
-	//SCENEMANAGER->changeScene("대사씬");
+	
+	switch (mode)
+	{
+	case 맵툴:
+
+		break;
+	case 타이틀:
+		SCENEMANAGER->changeScene("타이틀");
+		break;
+	case 다이얼로그:
+		SCENEMANAGER->changeScene("대사씬");
+		break;
+	case 도그본:
+		SCENEMANAGER->changeScene("도그본");
+		break;
+	case 플레이어:
+		break;
+	case 기타추가하셈:
+		break;
+	default:
+		break;
+	}
 
 	return S_OK;
 }
@@ -36,14 +59,15 @@ void playGround::release(void)
 void playGround::update(void)	
 {
 	gameNode::update();
-	_mapTool->update();
-	//_player->update();
-	//SCENEMANAGER->update();
 
-	/*if (KEYMANAGER->isOnceKeyDown(VK_F1))
+	_mapTool->update();
+	_player->update();
+	SCENEMANAGER->update();
+
+	if (KEYMANAGER->isOnceKeyDown(VK_F1))
 	{
 		SOUNDMANAGER->play("title");
-	}*/
+	}
 
 	if (KEYMANAGER->isStayKeyDown('D')) CAMERAMANAGER->setCameraX2(CAMERAMANAGER->getCameraX2() + 5);
 	if (KEYMANAGER->isStayKeyDown('S')) CAMERAMANAGER->setCameraY2(CAMERAMANAGER->getCameraY2() + 5);
@@ -58,17 +82,36 @@ void playGround::render(void)
 	//흰색도화지 한 장 깔아둔다
 	//PatBlt(DC, 0, 0, WINSIZEX, WINSIZEY, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
 	PatBlt(DC, 0, 0, WINSIZEX, WINSIZEY, WHITENESS); // 카메라 매니저 DC -> getMemDC 로 바꾸었습니다.
-	PatBlt(DC2, 0, 0, BACKGROUNDSIZEX, BACKGROUNDSIZEY, WHITENESS);
+	
 	//============== 이 위로는 건드리지 말자 ==============
 	
-	_mapTool->render();
-	//_player->render();
-	
-	//SCENEMANAGER->render();
+	switch (mode)
+	{
+	case 맵툴:
+		PatBlt(DC2, 0, 0, BACKGROUNDSIZEX, BACKGROUNDSIZEY, WHITENESS);
+		_mapTool->render();
+		CAMERAMANAGER->getCameraDC2()->render(DC, 0, 0, CAMERAMANAGER->getCameraRc2().left, CAMERAMANAGER->getCameraRc2().top, CAMERA2X, CAMERA2Y);
+		break;
+	case 타이틀:
+		SCENEMANAGER->render();
+		break;
+	case 다이얼로그:
+		SCENEMANAGER->render();
+		break;
+	case 도그본:
+		SCENEMANAGER->render();
+		break;
+	case 플레이어:
+		_player->render();
+		break;
+	case 기타추가하셈:
+		break;
+	default:
+		break;
+	}
 
 	//================이 밑으로도 건드리지 말자 =============
 	
-	CAMERAMANAGER->getCameraDC2()->render(DC, 0, 0, CAMERAMANAGER->getCameraRc2().left, CAMERAMANAGER->getCameraRc2().top, CAMERA2X, CAMERA2Y);
 	IMAGEMANAGER->render("cursor", DC, _ptMouse.x, _ptMouse.y);
 	CAMERAMANAGER->getCameraDC()->render(getHDC() , 0 , 0 , CAMERAMANAGER->getCameraRc().left, CAMERAMANAGER->getCameraRc().top, CAMERAMANAGER->getCameraRc().right, CAMERAMANAGER->getCameraRc().bottom); 
 	
