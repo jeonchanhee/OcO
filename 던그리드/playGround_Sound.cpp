22684928c6c9d2057c93850852f0c83b63f72719
주 효원 <222kg@naver.com>
2018-07-12 19:03:26 +0900

    사운드

--- 던그리드/playGround_Sound.cpp
+++ 던그리드/playGround_Sound.cpp
@@ -14,5 +14,26 @@ HRESULT playGround::Sound_init(void)
 
 	//====================   PLAYER =================================
 
+
+	//==================== ENEMY =================================
+	SOUNDMANAGER->addSound("bat", "sound/enemy/bat.wav", false, false);
+	SOUNDMANAGER->addSound("bat2", "sound/enemy/bat2.wav", false, false);
+	SOUNDMANAGER->addSound("bat3", "sound/enemy/bat3.wav", false, false);
+	SOUNDMANAGER->addSound("batFire", "sound/enemy/batFire.wav", false, false);
+	SOUNDMANAGER->addSound("bossChest", "sound/enemy/bossChest.wav", false, false);
+	SOUNDMANAGER->addSound("bossLaughter", "sound/enemy/bossLaughter.wav", false, false);
+	SOUNDMANAGER->addSound("dropBomb", "sound/enemy/dropBomb.wav", false, false);
+	SOUNDMANAGER->addSound("enemySound", "sound/enemy/enemySound.wav", false, false);
+	SOUNDMANAGER->addSound("fairy", "sound/enemy/fairy.wav", false, false);
+	SOUNDMANAGER->addSound("fireBullet", "sound/enemy/fireBullet.wav", false, false);
+	SOUNDMANAGER->addSound("growl", "sound/enemy/growl.wav", false, false);
+	SOUNDMANAGER->addSound("mosterDie", "sound/enemy/mosterDie.wav", false, false);
+	SOUNDMANAGER->addSound("musicAngel", "sound/enemy/musicAngel.wav", false, false);
+	SOUNDMANAGER->addSound("spawnMonster", "sound/enemy/spawnMonster.wav", false, false);
+	SOUNDMANAGER->addSound("음악요정공격", "sound/enemy/.wav", false, false);
+
+
+
+
 	return S_OK;
 }
